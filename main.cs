74f68ed523929cf27501fca500b08a716b7a730a
Jojo Liam <162688391+dibblesdribbles@users.noreply.github.com>
2024-03-08 11:11:22 +0200

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,33 @@
+ public const string blankAdditionalNamespace = "";
+
+        const string k_NameForNullType = "None";
+
+        public UsableType (Type usableType)
+        {
+            type = usableType;
+
+            if (type != null)
+            {
+                name = usableType.Name;
+                nameWithSorting = name.ToUpper ()[0] + "/" + name;
+                additionalNamespace = unrequiredNamespaces.All (t => usableType.Namespace != t) ? usableType.Namespace : blankAdditionalNamespace;
+            }
+            else
+            {
+                name = k_NameForNullType;
+                nameWithSorting = k_NameForNullType;
+                additionalNamespace = blankAdditionalNamespace;
+            }
+
+            public int CompareTo (object obj)
+        {
+            if (obj == null)
+                return 1;
+
+            UsableType other = (UsableType)obj;
+            
+            if(other == null)
+                throw new ArgumentException("This object is not a UsableType.");
+
+            return name.ToLower().CompareTo (other.name.ToLower());
+        }
